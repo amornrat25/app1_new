ca31983283c5efe9f43da65353519d430724adce
amornrat25 <s6703052411279@email.kmutnb.ac.th>
2025-12-25 11:10:51 +0700

    func-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './style.css';
 import React from 'react';
+import { Content, Footer, Header } from './func-components';
 
 function App() {
   return (
-    <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
-    </>
+   [<Header/>,<Content/>,<Footer/>]
+   
   )    
 }
 
