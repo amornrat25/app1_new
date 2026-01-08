e9510d414a909bffda3b44deeed778a2ba312273
amornrat25 <s6703052411279@email.kmutnb.ac.th>
2026-01-08 12:07:07 +0700

    evet data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Calculator2 } from './Calculator';
 import Button from './class-components';
+import { EventData2 } from './event-data';
 
 
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
        
 }
 
