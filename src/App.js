ee61e444337d00d420f9510ad6037aaa531683f3
amornrat25 <s6703052411279@email.kmutnb.ac.th>
2026-01-08 10:25:29 +0700

    Problems

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import Calendar from './class-components';
-import { Content, Footer, Header } from './func-components';
+import Button from './class-components';
+
+
 
 function App() {
-  return (
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>  
-   
-  )    
+  return <Button/>
+       
 }
 
 export default App;
