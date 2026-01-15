12b8361987cdb94757509d7e9a5db533203ba12b
amornrat25 <s6703052411279@email.kmutnb.ac.th>
2026-01-15 11:18:08 +0700

    State

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Calculator2 } from './Calculator';
 import Button from './class-components';
 import { EventData2 } from './event-data';
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 
 
 function App() {
-  return <RefsArray/>
+  return <MessageBox/>
        
 }
 
