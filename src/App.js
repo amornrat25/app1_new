805cd6429d846e331c5185861eda3abf1c9ee71b
amornrat25 <s6703052411279@email.kmutnb.ac.th>
2026-01-15 10:29:02 +0700

    References

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Calculator2 } from './Calculator';
 import Button from './class-components';
 import { EventData2 } from './event-data';
+import RefsArray from './refs-array';
 
 
 
 function App() {
-  return <EventData2/>
+  return <RefsArray/>
        
 }
 
